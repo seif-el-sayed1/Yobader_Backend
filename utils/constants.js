af670452311e5797008ad50b837f688807663b28
seif_elsayed <seifsayed121@gmail.com>
2026-09-01 01:31:41 +0300

    constants

---
+++ utils/constants.js
@@ -0,0 +1,7 @@
+exports.SUPER_ADMIN = "SUPER_ADMIN";
+exports.ADMIN = "ADMIN";
+exports.USER = "USER";
+
+exports.ROLES = [exports.SUPER_ADMIN, exports.ADMIN, exports.USER];
+
+exports.LOGIN_TYPE = ["APPLE", "GOOGLE", "EMAIL", "SOCIAL"];
