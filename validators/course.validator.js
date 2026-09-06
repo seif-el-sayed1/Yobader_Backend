3c820e2130d193cadb9632a3570238bb45280b48
seif_elsayed <seifsayed121@gmail.com>
2026-09-06 21:37:15 +0300

    validate update course

--- validators/course.validator.js
+++ validators/course.validator.js
@@ -51,6 +51,43 @@ class CourseValidator {
         joiErrorHandler(schema, req);
         next();
     })
+
+    validateUpdateCourse = asyncHandler(async (req, res, next) => {
+        const schema = Joi.object({
+            title: Joi.string().optional(),
+            description: Joi.string().optional(),
+            slug: Joi.string().optional(),
+            price: Joi.number().optional(),
+            image: Joi.string().optional(),
+            courseLevel: Joi.string().valid(...COURSE_LEVELS).optional().messages({
+                "any.only": `Course Level must be one of ${COURSE_LEVELS.join(", ")}`,
+            }),
+            startDate: Joi.date().optional(),
+            isFree: Joi.boolean().optional(),
+            isPublished: Joi.boolean().optional(),
+            hasDiscount: Joi.boolean().optional(),
+            discountPercent: Joi.number()
+                .min(10)
+                .max(100)
+                .when("hasDiscount", {
+                    is: true,
+                    then: Joi.required().messages({
+                        "any.required": "Discount Percent is required",
+                    }),
+                    otherwise: Joi.forbidden(),
+                })
+                .messages({
+                    "number.min": "Discount Percent must be at least 10",
+                    "number.max": "Discount Percent cannot exceed 100",
+                }),
+            priceAfterDiscount: Joi.forbidden(),
+        }).min(1).messages({
+            "object.min": "At least one field must be provided to update",
+        });
+
+        joiErrorHandler(schema, req);
+        next();
+    })
 }
 
 module.exports = new CourseValidator();
